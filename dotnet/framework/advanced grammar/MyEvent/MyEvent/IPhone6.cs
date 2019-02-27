fcb8d48c3c4e30f96c71b6a48cc1d13fd2cc2475
TenfyGit <15895920538@163.com>
2019-02-27 17:31:00 +0800

    e

---
+++ dotnet/framework/advanced grammar/MyEvent/MyEvent/IPhone6.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyEvent
+{
+    public delegate void PriceChangedHandle(decimal oldPrice,decimal newPrice);
+    public class IPhone6
+    {
+        private decimal price;
+        public event PriceChangedHandle PriceChanged;
+        public decimal Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                if (price == value)
+                {
+                    return;
+                }
+                decimal oldPrice = price;
+                price = value;
+                //如果調用列表不為空,則觸發
+                if (PriceChanged != null)
+                {
+                    PriceChanged(oldPrice, price);
+                }
+            }
+        }
+    }
+}
