357fd896b6fa801a102a96da63a7dd25effbcbce
TenfyGit <15895920538@163.com>
2019-03-10 22:26:18 +0800

    etc

--- dotnet/core/asp.net/simple/DistributedCacheDemo/DistributedCacheDemo/Startup.cs
+++ dotnet/core/asp.net/simple/DistributedCacheDemo/DistributedCacheDemo/Startup.cs
@@ -5,7 +5,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -32,7 +31,11 @@ namespace DistributedCacheDemo
             });
 
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            //services.AddDistributedSqlServerCache(options =>
+            //{
+            //    options.SystemClock = new 
+            //});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,11 +48,8 @@ namespace DistributedCacheDemo
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
