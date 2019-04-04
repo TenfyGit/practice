855ad3bb1740839c9b47c173a3027d57361cb005
TenfyGit <15895920538@163.com>
2019-04-04 19:55:39 +0800

    egt

---
+++ web/js/twle/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using twle.Models;
+
+namespace twle.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Introduct()
+        {
+            return View();
+        }
+        public IActionResult IsNaN()
+        {
+            return View();
+        }
+        public IActionResult Output()
+        {
+            return View();
+        }
+    }
+}
